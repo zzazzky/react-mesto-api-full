aed97cfa436227cf1f8a64056420f59353b84d1e
Victoria Gerasimova <zzazzky@gmail.com>
2023-02-03 20:04:53 +0500

    в CORS добавлено разрешение куки

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,13 +1,23 @@
+const allowdedCors = [
+  'https://frontend.mesto.gerasimova.nomoredomainsclub.ru',
+  'http://frontend.mesto.gerasimova.nomoredomainsclub.ru',
+  'http://localhost:3000',
+];
+
 function cors(req, res, next) {
+  const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  if (allowdedCors.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.setHeader('Access-Control-Allow-Headers', requestHeaders);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
   }
   next();
 }
