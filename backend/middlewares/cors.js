b087c4b8f67aa97a6d8bfc27687ed5cad802aca4
Victoria Gerasimova <zzazzky@gmail.com>
2023-02-03 19:51:22 +0500

    feat: добавлена обработка запросов с любых доменов

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,9 @@
-const allowdedCors = [
-  'https://frontend.mesto.gerasimova.nomoredomainsclub.ru',
-  'http://frontend.mesto.gerasimova.nomoredomainsclub.ru',
-  'http://localhost:3000',
-];
-
 function cors(req, res, next) {
-  const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowdedCors.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   if (method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
