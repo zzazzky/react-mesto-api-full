4f238c5c67c76c603fd3527cf320c2e8d90b7350
Victoria Gerasimova <zzazzky@gmail.com>
2023-02-04 14:13:29 +0500

    fix: Исправлена ошибка настройки CORS

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,16 +8,14 @@ function cors(req, res, next) {
   const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowdedCors.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.setHeader('Access-Control-Allow-Headers', requestHeaders);
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   }
   next();
 }
